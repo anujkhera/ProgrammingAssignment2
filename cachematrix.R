25f5afd6e2c54e0db6966787637f899a06124f5b
Anuj Khera <anujkhera@in.com>
2014-11-23 08:25:20 +0530

    final push to github from my local machine

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,33 @@
-New File Created 
+#New File Created 
+
+makeCacheMatrix <- function(x = matrix()) {
+    test<- NULL
+    set <- function(y) {
+        x <<- y
+        
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(solve) 
+        getinverse <- function() test
+    
+    
+}
+
+
+# inverse matrix function. 
+
+cacheSolve <- function(x, ...) {
+    
+    test <- x$getinverse()
+    
+    
+    if(!is.null(test)) {
+        
+        return(test)
+    }
+    
+}
+
+# Modified by myself @local machine
